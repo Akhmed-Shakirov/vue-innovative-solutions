c035351276d1ca37d3b94911cc04cb0485bc2b98
Akhmed <id.shakirov.akhmed@gmail.com>
2024-01-24 16:49:46 +0600

    Fix table

--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tables">
+    <div class="tables" :class="{ 'tables-padding' : isPadding }">
         <div class="tables__haed">
             <div class="tables__block">
                 <h3>Title</h3>
@@ -70,105 +70,192 @@
 import { ref, computed } from 'vue'
 
 const isFull = ref<boolean>(false)
+const isPadding = ref<boolean>(false)
+
 const data = ref<any[]>([
+	{
+		"name": "Aquila Campbell",
+		"phone": "(737) 417-3624",
+		"email": "enim@outlook.com",
+		"address": "Ap #318-7959 Consectetuer Rd.",
+		"postalZip": "631210",
+		"region": "Calabarzon",
+		"country": "Nigeria",
+		"list": 17,
+		"text": "ut mi. Duis risus odio, auctor vitae, aliquet nec, imperdiet",
+		"numberrange": 8,
+		"currency": "$47.71",
+		"alphanumeric": "ZLE46OFK1TK"
+	},
+	{
+		"name": "Ebony Grimes",
+		"phone": "(286) 689-5354",
+		"email": "faucibus.leo@google.com",
+		"address": "341-4915 Dictum Rd.",
+		"postalZip": "134518",
+		"region": "Berlin",
+		"country": "Chile",
+		"list": 11,
+		"text": "tempus risus. Donec egestas. Duis ac arcu. Nunc mauris. Morbi",
+		"numberrange": 9,
+		"currency": "$45.57",
+		"alphanumeric": "QFE41YRI5VW"
+	},
+	{
+		"name": "Laith Macdonald",
+		"phone": "(311) 427-5409",
+		"email": "sed@hotmail.edu",
+		"address": "Ap #903-2372 Mauris Rd.",
+		"postalZip": "10508",
+		"region": "Marche",
+		"country": "Austria",
+		"list": 13,
+		"text": "ultricies adipiscing, enim mi tempor lorem, eget mollis lectus pede",
+		"numberrange": 3,
+		"currency": "$70.38",
+		"alphanumeric": "OJE38BHI3VC"
+	},
+	{
+		"name": "Candice Byers",
+		"phone": "1-889-571-6773",
+		"email": "tincidunt@icloud.net",
+		"address": "Ap #814-2804 Ut St.",
+		"postalZip": "32-78",
+		"region": "North Region",
+		"country": "Singapore",
+		"list": 5,
+		"text": "luctus sit amet, faucibus ut, nulla. Cras eu tellus eu",
+		"numberrange": 1,
+		"currency": "$88.46",
+		"alphanumeric": "ERR06CZL1KI"
+	},
+	{
+		"name": "MacKenzie Moon",
+		"phone": "1-236-459-3839",
+		"email": "eleifend.vitae@icloud.org",
+		"address": "Ap #340-932 Mi Avenue",
+		"postalZip": "9477",
+		"region": "Central Kalimantan",
+		"country": "United States",
+		"list": 17,
+		"text": "Sed nulla ante, iaculis nec, eleifend non, dapibus rutrum, justo.",
+		"numberrange": 5,
+		"currency": "$86.77",
+		"alphanumeric": "JNP36WRB6DR"
+	},
     {
-        body1: 'body1 - 1 body1 - 1',
-        body2: 'body2 - 1 body2 - 1',
-        body3: 'body3 - 1 body3 - 1',
-        body4: 'body4 - 1 body4 - 1',
-        body5: 'body5 - 1 body5 - 1',
-        body6: 'body6 - 1 body6 - 1',
-        body7: 'body7 - 1 body7 - 1',
-        body8: 'body8 - 1 body8 - 1',
-        body9: 'body9 - 1 body9 - 1',
-        body10: '',
-        body11: '',
-        body12: '',
-        body13: '',
-    },
-    {
-        body1: 'body1 - 2',
-        body2: 'body2 - 2',
-        body3: 'body3 - 2',
-        body4: 'body4 - 2 body4 - 2 body4 - 2',
-        body5: 'body5 - 2 body5 - 2 body5 - 2 body5 - 2 body5 - 2',
-        body6: 'body6 - 2',
-        body7: 'body7 - 2',
-        body8: 'body8 - 2',
-        body9: 'body9 - 2',
-        body10: '',
-        body11: '',
-        body12: '',
-        body13: '',
-    },
-    {
-        body1: 'body1 - 3',
-        body2: 'body2 - 3',
-        body3: 'body3 - 3',
-        body4: 'body4 - 3',
-        body5: 'body5 - 3',
-        body6: 'body6 - 3',
-        body7: 'body7 - 3',
-        body8: 'body8 - 3',
-        body9: 'body9 - 3',
-        body10: '',
-        body11: '',
-        body12: '',
-        body13: '',
-    },
+		"name": "Inez Benson",
+		"phone": "(492) 432-5866",
+		"email": "malesuada.id@aol.couk",
+		"address": "Ap #785-1406 Enim. Rd.",
+		"postalZip": "37672",
+		"region": "Dōngběi",
+		"country": "New Zealand",
+		"list": 5,
+		"text": "tortor nibh sit amet orci. Ut sagittis lobortis mauris. Suspendisse",
+		"numberrange": 3,
+		"currency": "$47.56",
+		"alphanumeric": "BKQ45RUS6LQ"
+	},
+	{
+		"name": "Oleg Vinson",
+		"phone": "(837) 553-5647",
+		"email": "diam.duis@outlook.couk",
+		"address": "869-1217 At St.",
+		"postalZip": "586611",
+		"region": "Tver Oblast",
+		"country": "Spain",
+		"list": 5,
+		"text": "et, lacinia vitae, sodales at, velit. Pellentesque ultricies dignissim lacus.",
+		"numberrange": 3,
+		"currency": "$4.75",
+		"alphanumeric": "EIY26BSS9KW"
+	},
+	{
+		"name": "Erasmus Mueller",
+		"phone": "1-939-381-4176",
+		"email": "mauris.eu.elit@google.edu",
+		"address": "344-3338 Senectus Rd.",
+		"postalZip": "999094",
+		"region": "Guanacaste",
+		"country": "Netherlands",
+		"list": 15,
+		"text": "luctus lobortis. Class aptent taciti sociosqu ad litora torquent per",
+		"numberrange": 3,
+		"currency": "$17.23",
+		"alphanumeric": "YLP75TNH1FG"
+	},
+	{
+		"name": "Alma Waller",
+		"phone": "(973) 743-5723",
+		"email": "lacus@hotmail.net",
+		"address": "Ap #958-9324 Tellus Rd.",
+		"postalZip": "39375",
+		"region": "Dōngběi",
+		"country": "South Korea",
+		"list": 7,
+		"text": "convallis est, vitae sodales nisi magna sed dui. Fusce aliquam,",
+		"numberrange": 3,
+		"currency": "$3.41",
+		"alphanumeric": "PXX17IBJ4ML"
+	},
+	{
+		"name": "Tana Vasquez",
+		"phone": "1-571-437-4632",
+		"email": "lacinia.orci@hotmail.edu",
+		"address": "9570 Orci. Rd.",
+		"postalZip": "14055",
+		"region": "İzmir",
+		"country": "Vietnam",
+		"list": 9,
+		"text": "quam vel sapien imperdiet ornare. In faucibus. Morbi vehicula. Pellentesque",
+		"numberrange": 6,
+		"currency": "$56.38",
+		"alphanumeric": "AYW92DHF0XY"
+	}
 ])
 const keys = ref<any[]>([
-    { name: 'body1', title: 'Р/М' },
-    { name: 'body2', title: 'Дата' },
-    { name: 'body3', title: 'Действия' },
-    // { name: 'body4', title: 'Круг' },
-    // { name: 'body5', title: 'Показания' },
-    // { name: 'body6', title: 'Потребление за период' },
-    // { name: 'body7', title: 'Фото' },
-    // { name: 'body8', title: 'Типичная ситуация' },
+    // { name: 'name', title: 'Имя' },
+    // { name: 'phone', title: 'Телефон' },
+    // { name: 'email', title: 'Почта' },
+    // { name: 'address', title: 'Адрес' },
+    // { name: 'postalZip', title: 'Индекс' },
+    // { name: 'region', title: 'Регион' },
+    // { name: 'country', title: 'Страна' },
+    // { name: 'list', title: 'Список' },
+    // { name: 'text', title: 'Текст' },
+    // { name: 'numberrange', title: 'Рейтинг' },
+    // { name: 'currency', title: 'Стоимость' },
+    // { name: 'alphanumeric', title: 'Артикул' },
+    
 
-    // { name: 'body9', title: 'Введено' }, 
-    // { name: 'body10', title: 'Ввел' }, 
-    // { name: 'body11', title: 'Основание' },
-
-    // { name: 'body12', title: 'Забракованно' }, 
-    // { name: 'body13', title: 'Забраковал' }, 
-
-    // { 
-    //     name: 'Новое значение', 
-    //     children: [ 
-    //         { name: 'body1', title: 'Р/М' },
-    //         { name: 'body2', title: 'Дата' },
-    //         { name: 'body3', title: 'Действия' },
-    //         { name: 'body4', title: 'Круг' },
-    //         { name: 'body5', title: 'Показания' },
-    //         { name: 'body6', title: 'Потребление за период' },
-    //         { name: 'body7', title: 'Фото' },
-    //         { name: 'body8', title: 'Типичная ситуация' },
-    //     ] 
-    // },
-    // { 
-    //     name: 'Назначение', 
-    //     children: [ 
-    //         { name: 'body9', title: 'Введено' }, 
-    //         { name: 'body10', title: 'Ввел' }, 
-    //         { name: 'body11', title: 'Основание' },
-    //     ] 
-    // },
-    // { 
-    //     name: 'Забраковка', 
-    //     children: [ 
-    //         { name: 'body12', title: 'Забракованно' }, 
-    //         { name: 'body13', title: 'Забраковал' }, 
-    //     ] 
-    // },
-    // { 
-    //     name: 'Забраковка', 
-    //     children: [ 
-    //         { name: 'body12', title: 'Забракованно' }, 
-    //         { name: 'body13', title: 'Забраковал' }, 
-    //     ] 
-    // },
+    {
+        name: 'Основное',
+        children: [
+            { name: 'name', title: 'Имя' },
+            { name: 'phone', title: 'Телефон' },
+            { name: 'email', title: 'Почта' },
+        ]
+    }, 
+    {
+        name: 'Место',
+        children: [
+            { name: 'address', title: 'Адрес' },
+            { name: 'postalZip', title: 'Индекс' },
+            { name: 'region', title: 'Регион' },
+            { name: 'country', title: 'Страна' },
+        ]
+    }, 
+    {
+        name: 'Описание',
+        children: [
+            { name: 'list', title: 'Список' },
+            { name: 'text', title: 'Текст' },
+            { name: 'numberrange', title: 'Рейтинг' },
+            { name: 'currency', title: 'Стоимость' },
+            { name: 'alphanumeric', title: 'Артикул' },
+        ]
+    },
 ])
 
 
@@ -230,7 +317,7 @@ const names = computed<any[]>(() => {
         border-spacing: 0;
         border-bottom: 1px solid #606060;
         
-        tr ~ tr > td, th {
+        tr > td, th {
             border-top: 1px solid #606060;
             border-right: 1px solid #606060;
         }
@@ -266,6 +353,23 @@ const names = computed<any[]>(() => {
             text-align: center;
             font-size: 15px;
             text-transform: uppercase;
+        }
+    }
+    
+    &-padding {
+        .tables__haed {
+            border-bottom: none;
+            margin-bottom: 10px;
+        }
+
+        .tables__body {
+            padding-inline: 16px;
+        }
+
+        .tables__main > tr > td, th {
+            &:first-child {
+                border-left: 1px solid #606060;
+            }
         }
     }
 }
